4bb620377ae1dcdc03e473c5b4b12d6730e6a5e3
Gideon <1702233@student.hu.nl>
2017-09-11 09:31:47 +0200

    Practical Exercises

---
+++ Les3/exercises.py
@@ -0,0 +1,15 @@
+age = 63
+hits = 10
+shield = 5
+report = "large bonuses are awarded to hardworking people"
+
+if age > 62:
+    print ("You can get social security benefits")
+
+if "large bonuses" in report:
+    print("Vacation time!")
+
+if hits > 10 and shield == 0:
+    print("You're dead...")
+
+
